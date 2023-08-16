9059783068f83719eadd5c3d6d2955062cc6f1d2
root <root@hayashi>
2023-08-17 00:38:05 +0700

    demo run server de test statefull cua octane

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+static $values = [];
+
+Route::get('/static', function () use (&$values) {
+    $values[] = time();
+    dd($values);
 });
